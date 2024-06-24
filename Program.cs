c51a24613af7cb0df2bbb7873cf3716e6b0d0544
fatihylmz1 <fatihylmz27@icloud.com>
2024-06-24 13:50:08 +0300

    variables-1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Merhaba");
+﻿
+Console.WriteLine("Merhaba");
+
+
+//Variable Types
+
+int sayi = 10; //tamsayı
+
+double kdv = 1.18; //ondalıklı sayı
+
+char gender = 'k'; //tek harf
+
+string cinsiyet = "kadın"; //kelime veya cümle
+
